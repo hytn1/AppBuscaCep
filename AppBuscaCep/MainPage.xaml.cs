bb9305bbaf8eb0cc147d0769ed969b0c1330bcd8
Usuario <Usuario@CPD03-M03>
2024-05-16 15:08:59 -0300

    app

--- AppBuscaCep/MainPage.xaml.cs
+++ AppBuscaCep/MainPage.xaml.cs
@@ -5,7 +5,45 @@ namespace AppBuscaCep
 {
     public partial class MainPage : ContentPage
     {
-       
-    }
+        int count = 0;
 
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Endereco dados_endereco = await DataService.GetEnderecoByCep(txt_cep.Text);
+
+                BindingContext = dados_endereco;
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            try
+            {
+                List<Bairro> arr_bairros = await DataService.GetBairrosByIdCidade(4874);
+
+                //Endereco dados_endereco = await DataService.GetEnderecoByCep(txt_cep.Text);
+
+                //BindingContext = dados_endereco;
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+
+        }
+    }
 }
--- AppBuscaCep/Services/DataService.cs
+++ AppBuscaCep/Services/DataService.cs
@@ -1,12 +1,145 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AppBuscaCep.Models;
+using Newtonsoft.Json;
 
 namespace AppBuscaCep.Services
 {
-    internal class DataService
+    public class DataService
     {
+        /**
+       * https://stackoverflow.com/questions/43115536/php-built-in-webserver-not-reacheable-with-ipport-via-network-cable
+       */
+        public static async Task<Endereco> GetEnderecoByCep(string cep)
+        {
+            Endereco end;
+
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "https://cep.metoda.com.br/endereco/by-cep?cep=" + cep;
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    end = JsonConvert.DeserializeObject<Endereco>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+
+            return end;
+        }
+
+
+        /**
+         * 
+         */
+        public static async Task<List<Bairro>> GetBairrosByIdCidade(int id_cidade)
+        {
+            List<Bairro> arr_bairros = new List<Bairro>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "https://cep.metoda.com.br/bairro/by-cidade?id_cidade=" + id_cidade;
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    arr_bairros = JsonConvert.DeserializeObject<List<Bairro>>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+
+            return arr_bairros;
+        }
+
+        /**
+         * 
+         */
+        public static async Task<List<Cidade>> GetCidadesByEstado(string uf)
+        {
+            List<Cidade> arr_cidades = new List<Cidade>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(
+                    "https://cep.metoda.com.br/cidade/by-uf?uf=" + uf);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.
+                        Content.ReadAsStringAsync().Result;
+
+                    arr_cidades = JsonConvert.DeserializeObject<
+                        List<Cidade>>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.
+                        Content.ToString());
+            }
+
+            return arr_cidades;
+        }
+
+        /**
+         * Obtem a lista de logradouros (ruas) de um bairro.
+         */
+        public static async Task<List<Logradouro>>
+            GetLogradourosByBairroAndIdCidade(
+            string bairro, int id_cidade)
+        {
+            List<Logradouro> arr_logradouros = new List<Logradouro>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(
+                    "https://cep.metoda.com.br/logradouro/by-bairro?id_cidade=" + id_cidade + "&bairro=" + bairro);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    arr_logradouros = JsonConvert.DeserializeObject<List<Logradouro>>(json);
+
+                }
+                else
+                    throw new Exception(response.RequestMessage.Content.ToString());
+            }
+
+            return arr_logradouros;
+        }
+
+        public static async Task<List<Cep>> GetCepsByLogradouro(
+            string logradouro)
+        {
+            List<Cep> arr_ceps = new List<Cep>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(
+               "https://cep.metoda.com.br/cep/by-logradouro?logradouro="
+                 + logradouro);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.
+                        Content.ReadAsStringAsync().Result;
+
+                    arr_ceps = JsonConvert.DeserializeObject<
+                        List<Cep>>(json);
+                }
+                else
+                    throw new Exception(response.RequestMessage.
+                        Content.ToString());
+            }
+
+            return arr_ceps;
+        }
     }
 }
+
